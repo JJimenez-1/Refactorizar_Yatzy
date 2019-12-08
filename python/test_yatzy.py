9c088d33617d171401d2c695b00093f83a0b99e6
JJimenez-1 <jjimenez@cifpfbmoll.eu>
2019-12-08 12:59:51 +0100

    test: Refactorizar nombres de casos test y código threes
    Hemos cambiado el nombre de algunos casos test, que no se entendian muy
    bien, a que función hacia referencia, y modificado el código del caso
    test_threes, siguiendo pep8

--- python/test_yatzy.py
+++ python/test_yatzy.py
@@ -15,22 +15,22 @@ def test_yatzy_scores_50():
         assert 0 == Yatzy.yatzy(6, 6, 6, 6, 3)
 
 
-def test_1s():
+def test_ones():
         assert 1 == Yatzy.ones(1, 2, 3, 4, 5)
         assert 2 == Yatzy.ones(1, 2, 1, 4, 5)
         assert 0 == Yatzy.ones(6, 2, 2, 4, 5)
         assert 4 == Yatzy.ones(1, 2, 1, 1, 1)
 
 
-def test_2s():
+def test_twos():
         assert 4 == Yatzy.twos(1, 2, 3, 2, 6)
         assert 10 == Yatzy.twos(2, 2, 2, 2, 2)
 
 
 def test_threes():
-        assert 6 == Yatzy.threes(1,2,3,2,3)
-        assert 12 == Yatzy.threes(2,3,3,3,3)
-  
+        assert 6 == Yatzy.threes(1, 2, 3, 2, 3)
+        assert 12 == Yatzy.threes(2, 3, 3, 3, 3)
+
 
 def test_fours_test():
         assert 12 == Yatzy(4,4,4,5,5).fours()
