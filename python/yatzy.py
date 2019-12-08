9adf3da34b37e3e5f15cdc61896d52588187dec6
JJimenez-1 <jjimenez@cifpfbmoll.eu>
2019-12-08 12:57:11 +0100

    refactor: Cambiar código función threes
    Hemos refactorizado la función threes

--- python/yatzy.py
+++ python/yatzy.py
@@ -25,20 +25,10 @@ class Yatzy:
 
 
     @staticmethod
-    def threes( d1,  d2,  d3,  d4,  d5):
-        s = 0
-        if (d1 == 3):
-             s += 3
-        if (d2 == 3):
-             s += 3
-        if (d3 == 3):
-             s += 3
-        if (d4 == 3):
-             s += 3
-        if (d5 == 3):
-             s += 3
-        return s
-    
+    def threes(*dice):
+        THREE = 3
+        return dice.count(THREE) * THREE
+
 
     def __init__(self, d1, d2, d3, d4, _5):
         self.dice = [0]*5
