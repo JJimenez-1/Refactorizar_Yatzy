ea1b096b88f85e8b5c9e377bfcf3074fe80d158d
JJimenez-1 <jjimenez@cifpfbmoll.eu>
2019-12-09 10:49:50 +0100

    test: Error caso test_smallStraight, borrado.
    Se ha vuelto a aplicar los casos test_smallStraight, ya que se habian
    borrado por equivocación. Se ha aplicado pep8, en estos.

--- python/test_yatzy.py
+++ python/test_yatzy.py
@@ -76,10 +76,10 @@ def test_four_of_a_kind():
 
 
 def test_smallStraight():
-        assert 15 == Yatzy.smallStraight(1,2,3,4,5)
-        assert 15 == Yatzy.smallStraight(2,3,4,5,1)
-        assert 0 == Yatzy.smallStraight(1,2,2,4,5)
-  
+        assert 15 == Yatzy.smallStraight(1, 2, 3, 4, 5)
+        assert 15 == Yatzy.smallStraight(2, 3, 4, 5, 1)
+        assert 0 == Yatzy.smallStraight(1, 2, 2, 4, 5)
+
 
 def test_largeStraight():
         assert 20 == Yatzy.largeStraight(6,2,3,4,5)
