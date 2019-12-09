e6f8573d6c14b6f6cd2b1128af72cf1fd3800838
JJimenez-1 <jjimenez@cifpfbmoll.eu>
2019-12-09 10:56:06 +0100

    test: Aplicar pep8 en caso test_largeStraight

--- python/test_yatzy.py
+++ python/test_yatzy.py
@@ -82,10 +82,10 @@ def test_smallStraight():
 
 
 def test_largeStraight():
-        assert 20 == Yatzy.largeStraight(6,2,3,4,5)
-        assert 20 == Yatzy.largeStraight(2,3,4,5,6)
-        assert 0 == Yatzy.largeStraight(1,2,2,4,5)
-  
+        assert 20 == Yatzy.largeStraight(6, 2, 3, 4, 5)
+        assert 20 == Yatzy.largeStraight(2, 3, 4, 5, 6)
+        assert 0 == Yatzy.largeStraight(1, 2, 2, 4, 5)
+
 
 def test_fullHouse():
         assert 18 == Yatzy.fullHouse(6,2,2,2,6)
