80b88f3622d8935a696352ab996845dbe00ed0f1
JJimenez-1 <jjimenez@cifpfbmoll.eu>
2019-12-10 12:09:07 +0100

    refactor: Borrar __init__
    Hemos borrado __init__ para pruebas posteriores

--- python/yatzy.py
+++ python/yatzy.py
@@ -1,13 +1,5 @@
 class Yatzy:
 
-    def __init__(self, d1, d2, d3, d4, _5):
-        self.dice = [0]*5
-        self.dice[0] = d1
-        self.dice[1] = d2
-        self.dice[2] = d3
-        self.dice[3] = d4
-        self.dice[4] = _5
-
     @staticmethod
     def chance(*dice):
         total = 0
